712907de1e6abc1d195eca8482e4b08923bd2f01
Cliff Rice <ce.rice@knights.ucf.edu>
2014-10-19 01:10:10 -0400

    First commit- three empty activities

---
+++ SmartNav/gen/cop4331/group16/smartnav/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cop4331.group16.smartnav;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SmartNav/src/cop4331/group16/smartnav/MapActivity.java
@@ -0,0 +1,13 @@
+package cop4331.group16.smartnav;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}

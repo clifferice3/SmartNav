195e8f6d2270c23432e9b899892ad1a4aec0b873
ryanz93 <RyanZ93@knights.ucf.edu>
2014-11-09 15:59:05 -0500

    I wrote a constructor that creates a list of markers and a GoogleMap. The drawMap function now clears the current markers, creates a list of new markers out of the addresses, adds the new markers to the map, and makes them visible. I also wrote the getCurrentLoc function. Any class in the package has access to the map. I have not been able to confirm this works since I’m still trying to get the emulator working, but I think it should work since its mostly function calls to Google’s API.

--- SmartNav/src/cop4331/group16/smartnav/APIWrapper.java
+++ SmartNav/src/cop4331/group16/smartnav/APIWrapper.java
@@ -2,16 +2,51 @@ package cop4331.group16.smartnav;
 
 import java.util.*;
 
+import com.google.android.maps.MapView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.LatLng;
+
+import 	android.location.Location;
+
 public class APIWrapper
 {
+    GoogleMap map;
+    private ArrayList<Marker> locs;
+
+    public APIWrapper()
+    {
+        map = MapView.getMap();
+        locs = new ArrayLst<Marker>();
+    }
+
 	public void drawMap(ArrayList<Address> addresses)
 	{
-		
+        //clear the map of all markers
+        for (Maker m : locs)
+        {
+            m.remove();
+        }
+
+        //remove markers from list of markers on map
+        locs.clear();
+
+        //add markers at each location on the map
+        for (Address a : addresses)
+        {
+            MarkerOptions tempOpt = new MarkerOptions();
+            tempOpt.position(new LatLng(a.getLatitude(), a.getLongitude()));
+            tempMarker = map.addMarker(tempOpt);
+            tempMarker.setVisible(true);
+            locs.add(tempMarker);
+        }
+
 	}
 	
 	public Address getCurrentLoc()
 	{
-		return null;
+		Location curLoc = map.getMyLocation();
+        return new Address(curLoc.getLatitude, curLoc.getLongitude());
 	}
 	
 	public ArrayList<String> getDirections(Address start, Address end)

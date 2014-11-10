5fd7a3afb51aa4cb3bc2e4c6b1e362613256347c
jasontiller3 <jasontiller@gmail.com>
2014-11-10 15:36:05 -0500

    test gitignore

--- DSLV_Library/gen/com/mobeta/android/dslv/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.mobeta.android.dslv;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- SmartNav/gen/cop4331/group16/smartnav/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cop4331.group16.smartnav;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}

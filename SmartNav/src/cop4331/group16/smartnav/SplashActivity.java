8f593d1da9a36a75692dbd294b98a27836fd5e2e
jasontiller3 <jasontiller@gmail.com>
2014-11-19 11:19:19 -0500

    tweaked colors

--- SmartNav/src/cop4331/group16/smartnav/SplashActivity.java
+++ SmartNav/src/cop4331/group16/smartnav/SplashActivity.java
@@ -9,13 +9,11 @@ import android.view.Window;
 public class SplashActivity extends Activity {
 
    // Splash screen timer
-   private static int SPLASH_TIME_OUT = 1500;
+   private static int SPLASH_TIME_OUT = 2000;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-       getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-       getActionBar().hide();
        setContentView(R.layout.activity_splash);
 
        new Handler().postDelayed(new Runnable() {

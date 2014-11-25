fcbdc8c4d0495554b040a2de7bf03bc29c70f451
edorundo <edorundo@gmail.com>
2014-11-24 22:35:02 -0500

    Added comments to Address

--- SmartNav/src/cop4331/group16/smartnav/Address.java
+++ SmartNav/src/cop4331/group16/smartnav/Address.java
@@ -1,17 +1,23 @@
 package cop4331.group16.smartnav;
 
+/**
+ * This class is used to represent a specific location that the user could visit.
+ */
 public class Address
 {
-	private String name;
-	private double latitude;
+	private String name;		// Name of the location
+	private double latitude;	// Latitude and longitude coordinates of the location
 	private double longitude;
 	
+	// Constructor
 	public Address(String n, double lat, double lon)
 	{
 		name = n;
 		latitude = lat;
 		longitude = lon;
 	}
+	
+	// Getter methods
 	
 	public String getName()
 	{
